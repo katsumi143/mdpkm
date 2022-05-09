4d58d084e0e908b066a696319021e365724ba433
Blookers <blookyl305@gmail.com>
2022-05-10 01:06:55 +1000

    Remove vite-plugin-compress

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import compress from 'vite-plugin-compress';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,7 +13,6 @@ export default defineConfig({
         }
     },
     plugins: [
-        react(),
-        compress()
+        react()
     ]
 });
