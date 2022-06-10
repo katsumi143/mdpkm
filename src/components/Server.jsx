4a48582527e4498293efe362c05620ca2eb9e317
Blookers <blookyl305@gmail.com>
2022-06-10 20:46:49 +1000

    Server adding woooo

---
+++ src/components/Server.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { PencilFill, Trash3Fill } from 'react-bootstrap-icons';
+
+import Grid from '/voxeliface/components/Grid';
+import Image from '/voxeliface/components/Image';
+import Button from '/voxeliface/components/Button';
+import Typography from '/voxeliface/components/Typography';
+export default function Server({ name, icon, motd, type, players, address, instanceId, acceptTextures }) {
+    const { t } = useTranslation();
+    return <Grid height="fit-content" padding={8} spacing={12} background="$secondaryBackground2" borderRadius={8} justifyContent="space-between" css={{
+        minWidth: '24rem'
+    }}>
+        <Grid spacing={12}>
+            <Image
+                src={icon ? icon.startsWith('data:') ? icon : `data:image/png;base64,${icon}` : 'img/icons/minecraft/unknown_server.png'}
+                size={46}
+                background="$secondaryBackground"
+                borderRadius={4}
+                css={{
+                    minWidth: 46,
+                    minHeight: 46,
+                    transition: 'all 250ms cubic-bezier(0.4, 0, 0.2, 1)',
+
+                    '&:hover': {
+                        minWidth: 64,
+                        minHeight: 64
+                    }
+                }}
+            />
+            <Grid height="100%" spacing={4} direction="vertical">
+                <Typography size=".9rem" color="$primaryColor" family="Nunito" margin="6px 0 0" lineheight={1} whitespace="nowrap">
+                    {name || 'Minecraft Server'}
+                    {acceptTextures === 1 &&
+                        <Typography size=".7rem" color="$secondaryColor" weight={300} family="Nunito" margin="4px 0 0 8px" lineheight={1}>
+                            Server Resource Pack Accepted
+                        </Typography>
+                    }
+                </Typography>
+                {motd ?
+                    <span dangerouslySetInnerHTML={{ __html: motd }} style={{
+                        fontSize: '.8rem',
+                        textAlign: 'center',
+                        fontFamily: 'Nunito'
+                    }}/>
+                :
+                    <Typography size=".8rem" color="$secondaryColor" weight={400} family="Nunito" lineheight={1}>
+                        {address || 'Unknown server'}
+                    </Typography>
+                }
+            </Grid>
+        </Grid>
+        <Grid spacing={8} alignItems="center">
+            <Grid height="100%" spacing={4} padding={4} direction="vertical" alignItems="end">
+                {players && <Typography size=".8rem" color="$secondaryColor" family="Nunito" spacing={4} lineheight={1}>
+                    {players.online}/{players.max} Players
+                </Typography>}
+                {type && <Typography size=".8rem" color="$secondaryColor" family="Nunito" spacing={4} lineheight={1}>
+                    {type}
+                </Typography>}
+            </Grid>
+            {instanceId && <React.Fragment>
+                <Button theme="secondary" disabled>
+                    <PencilFill/>
+                    {t('app.mdpkm.common:actions.edit')}
+                </Button>
+                <Button theme="secondary" disabled>
+                    <Trash3Fill/>
+                    {t('app.mdpkm.common:actions.delete')}
+                </Button>
+            </React.Fragment>}
+        </Grid>
+    </Grid>;
+}
--- src/components/ServerManagement.jsx
+++ src/components/ServerManagement.jsx
@@ -2,41 +2,89 @@ import React, { useState, useEffect } from 'react';
 import nbt from 'nbt';
 import { useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
-import { PlusLg, PencilFill, Trash3Fill, ArrowClockwise } from 'react-bootstrap-icons';
+import { XLg, PlusLg, ArrowClockwise } from 'react-bootstrap-icons';
 
 import Grid from '/voxeliface/components/Grid';
-import Image from '/voxeliface/components/Image';
+import Server from './Server';
 import Button from '/voxeliface/components/Button';
+import Portal from '/voxeliface/components/Portal';
+import Header from '/voxeliface/components/Typography/Header';
+import Spinner from '/voxeliface/components/Spinner';
 import TextInput from '/voxeliface/components/Input/Text';
 import Typography from '/voxeliface/components/Typography';
+import InputLabel from '/voxeliface/components/Input/Label';
 import BasicSpinner from '/voxeliface/components/BasicSpinner';
 import TextTransition from './Transition/Text';
 
 import Util from '../common/util';
-
 export default function ServerManagement({ instanceId }) {
     const { t } = useTranslation();
     const instance = useSelector(state => state.instances.data.find(i => i.id === instanceId));
+    const [data, setData] = useState();
     const [items, setItems] = useState();
     const [filter, setFilter] = useState('');
+    const [addingInfo, setAddingInfo] = useState();
+    const [addingName, setAddingName] = useState('');
+    const [addingServer, setAddingServer] = useState();
+    const [addingAddress, setAddingAddress] = useState('');
+    const [addingAddress2, setAddingAddress2] = useState();
+    const resetAdding = () => {
+        setAddingName('');
+        setAddingAddress('');
+        setAddingAddress2();
+    };
+    const closeAdding = () => {
+        resetAdding();
+        setAddingServer(false);
+    };
+    const openAdding = () => {
+        resetAdding();
+        setAddingServer(true);
+    };
+    const addServer = () => {
+        closeAdding();
+        data.value.servers.value.value.push({
+            ip: { type: 'string', value: addingAddress },
+            name: { type: 'string', value: addingName || 'Minecraft Server' },
+            icon: { type: 'string', value: addingInfo?.icon?.replace?.('data:image/png;base64,', '') }
+        });
+        setData(data);
+        setItems(data.value.servers.value.value);
+        console.log([...new Uint8Array(nbt.writeUncompressed(data))]);
+        Util.writeBinaryFile(`${instance.path}/servers.dat`, [...new Uint8Array(nbt.writeUncompressed(data))]);
+    };
     useEffect(() => {
         if(!items) {
             if(!instance?.path)
                 return;
             const path = `${instance.path}/servers.dat`;
+            setData();
             setItems('loading');
             Util.fileExists(path).then(exists => {
                 if (exists)
                     Util.readBinaryFile(path).then(data => nbt.parse(data, (error, data) => {
                         if(error) throw error;
+                        setData(data);
                         setItems(data.value.servers.value.value);
                     }));
-                else
+                else {
+                    setData();
                     setItems([]);
+                }
             });
         }
     }, [items]);
+    useEffect(() => {
+        if (addingAddress2) {
+            setAddingInfo('loading');
+            Util.makeRequest(`https://api.mcsrvstat.us/2/${encodeURIComponent(addingAddress2)}`).then(data => {
+                setAddingInfo(data);
+            }).catch(() => setAddingInfo());
+        } else
+            setAddingInfo();
+    }, [addingAddress2]);
     useEffect(() => setItems(), [instanceId]);
+    console.log(data, addingInfo);
     return <React.Fragment>
         <Grid spacing={8} padding="4px 0" justifyContent="space-between">
             <Grid direction="vertical">
@@ -65,8 +113,8 @@ export default function ServerManagement({ instanceId }) {
                     {items === 'loading' ? <BasicSpinner size={16}/> : <ArrowClockwise size={14}/>}
                     {t('app.mdpkm.common:actions.refresh')}
                 </Button>
-                <Button theme="accent" disabled>
-                    <PlusLg/>
+                <Button theme="accent" onClick={openAdding} disabled={!data}>
+                    <PlusLg size={14}/>
                     {t('app.mdpkm.server_management.add')}
                 </Button>
             </Grid>
@@ -76,53 +124,74 @@ export default function ServerManagement({ instanceId }) {
                 ip?.value.toLowerCase().includes(filter) ||
                 name?.value.toLowerCase().includes(filter)
             ).map((item, index) =>
-                <Grid key={index} padding={8} spacing={12} alignItems="center" background="$secondaryBackground2" borderRadius={8} css={{
-                    position: 'relative'
-                }}>
-                    <Image
-                        src={item.icon ? `data:image/png;base64,${item.icon.value}` : 'img/icons/minecraft/unknown_server.png'}
-                        size={46}
-                        background="$secondaryBackground"
-                        borderRadius={4}
-                        css={{
-                            minWidth: 46,
-                            minHeight: 46,
-                            transition: 'all 250ms cubic-bezier(0.4, 0, 0.2, 1)',
-
-                            '&:hover': {
-                                minWidth: 64,
-                                minHeight: 64
-                            }
-                        }}
-                    />
-                    <Grid spacing={4} direction="vertical">
-                        <Typography size=".9rem" color="$primaryColor" family="Nunito" lineheight={1}>
-                            {item.name?.value}
-                            {item.acceptTextures?.value === 1 &&
-                                <Typography size=".7rem" color="$secondaryColor" weight={300} family="Nunito" margin="4px 0 0 8px" lineheight={1}>
-                                    Server Resource Pack Accepted
-                                </Typography>
-                            }
-                        </Typography>
-                        <Typography size=".8rem" color="$secondaryColor" weight={400} family="Nunito" lineheight={1}>
-                            {item.ip?.value}
-                        </Typography>
-                    </Grid>
-                    <Grid spacing={8} css={{
-                        right: 16,
-                        position: 'absolute'
-                    }}>
-                        <Button theme="secondary" disabled>
-                            <PencilFill/>
-                            {t('app.mdpkm.common:actions.edit')}
-                        </Button>
-                        <Button theme="secondary" disabled>
-                            <Trash3Fill/>
-                            {t('app.mdpkm.common:actions.delete')}
-                        </Button>
-                    </Grid>
-                </Grid>
+                <Server
+                    key={index}
+                    name={item.name?.value}
+                    icon={item.icon?.value}
+                    address={item.ip?.value}
+                    instanceId={instanceId}
+                    acceptTextures={item.acceptTextures?.value}
+                />
             )}
         </Grid>
+        {addingServer && <Portal>
+            <Grid width="100vw" height="100vh" direction="vertical" alignItems="center" background="#00000099" justifyContent="center" css={{
+                top: 0,
+                left: 0,
+                zIndex: 100000,
+                position: 'absolute'
+            }}>
+                <Grid padding={12} direction="vertical" background="$secondaryBackground" borderRadius={8} css={{
+                    border: '1px solid $secondaryBorder2',
+                    position: 'relative'
+                }}>
+                    <Header>
+                        Adding Minecraft Server
+                        <Typography size=".7rem" color="$secondaryColor" weight={400} family="Nunito">
+                            (may not appear if game is running)
+                        </Typography>
+                    </Header>
+                    <Grid spacing="2rem" justifyContent="space-between">
+                        <Grid direction="vertical">
+                            <InputLabel>Display name</InputLabel>
+                            <TextInput
+                                value={addingName}
+                                onChange={setAddingName}
+                                placeholder="Type a name"
+                            />
+
+                            <InputLabel spacious>Server address</InputLabel>
+                            <TextInput
+                                value={addingAddress}
+                                onBlur={() => setAddingAddress2(() => addingAddress)}
+                                onChange={setAddingAddress}
+                                placeholder="Type an IP Address"
+                            />
+                            <Grid margin="2rem 0 0" spacing={8}>
+                                <Button theme="accent" onClick={addServer} disabled={!addingAddress}>
+                                    <PlusLg size={14}/>
+                                    Add Server
+                                </Button>
+                                <Button theme="secondary" onClick={closeAdding}>
+                                    <XLg/>
+                                    Cancel
+                                </Button>
+                            </Grid>
+                        </Grid>
+                        <Grid height="fit-content" spacing="1rem" alignItems="center">
+                            {addingInfo === 'loading' && <Spinner/>}
+                            <Server
+                                name={addingName}
+                                icon={addingInfo?.icon}
+                                motd={addingInfo?.motd?.html?.join('</br>')}
+                                type={addingInfo ? `${addingInfo?.software ?? ''} ${addingInfo?.version ?? ''}` : null}
+                                players={addingInfo?.players}
+                                address={addingAddress}
+                            />
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Portal>}
     </React.Fragment>;
 };
