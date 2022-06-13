8ec381d8f012c7e8136d00cba11601e7e122606a
Blookers <blookyl305@gmail.com>
2022-06-13 16:37:23 +1000

    Default skins

--- src/common/slices/skins.js
+++ src/common/slices/skins.js
@@ -1,13 +1,23 @@
-import Util from '../util';
+import { Buffer } from 'buffer/';
 import { appDir } from '@tauri-apps/api/path';
 import { createSlice } from '@reduxjs/toolkit';
+
+import Util from '../util';
 
 const skinsPath = `${await appDir()}/skins.json`;
 const skins = await Util.readTextFile(skinsPath).then(JSON.parse).catch(console.warn);
 export const skinsSlice = createSlice({
     name: 'skins',
     initialState: {
-        data: skins?.data ?? []
+        data: skins?.data ?? [{
+            name: 'Steve',
+            image: await fetch('/img/skins/CLASSIC.png').then(r => r.arrayBuffer()).then(v => Buffer.from(v).toString('base64')),
+            variant: 'CLASSIC'
+        }, {
+            name: 'Alex',
+            image: await fetch('/img/skins/SLIM.png').then(r => r.arrayBuffer()).then(v => Buffer.from(v).toString('base64')),
+            variant: 'SLIM'
+        }]
     },
     reducers: {
         addSkin: (state, { payload }) => {
