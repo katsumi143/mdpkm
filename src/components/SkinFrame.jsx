1178f690c1db51eb245afe92e86203996b5c7b4b
Blookers <blookyl305@gmail.com>
2022-08-29 18:23:37 +1000

    Add Static Skin Rendering

--- src/components/SkinFrame.jsx
+++ src/components/SkinFrame.jsx
@@ -5,58 +5,89 @@ import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass.js';
 import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
 import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
 import { SkinViewer, PlayerAnimation } from 'skinview3d';
-export default function SkinFrame({ skin, cape, walk, zoom = false, model = 'auto-detect', width, height, control = false, stillWalk, background }) {
+export default function SkinFrame({ skin, cape, walk, zoom = false, image = false, model = 'auto-detect', width, height, control = false, stillWalk, background }) {
     const canvas = React.useRef();
     React.useLayoutEffect(() => {
-        const skinViewer = new SkinViewer({
-            width,
-            height,
-            canvas: canvas.current
-        });
-        if (skin)
-            skinViewer.loadSkin(skin, {
-                model
+        try {
+            const skinViewer = new SkinViewer({
+                width,
+                height,
+                canvas: image ? undefined : canvas.current,
+                renderPaused: image
             });
-        if (cape)
-            skinViewer.loadCape(cape);
-        skinViewer.fov = 8;
+            if (skin && !image)
+                skinViewer.loadSkin(skin, {
+                    model
+                });
+            if (cape && !image)
+                skinViewer.loadCape(cape);
+            skinViewer.fov = 8;
+            
+            const composer = new EffectComposer(skinViewer.renderer);
+            const renderPass = new RenderPass(skinViewer.scene, skinViewer.camera);
+            const fxaaPass = new ShaderPass(FXAAShader);
+            composer.addPass(renderPass);
+            composer.addPass(fxaaPass);
+
+            composer.setSize(width, height);
+            const pixelRatio = skinViewer.renderer.getPixelRatio();
+            composer.setPixelRatio(pixelRatio);
+            fxaaPass.material.uniforms['resolution'].value.x = 1 / (width * pixelRatio);
+            fxaaPass.material.uniforms['resolution'].value.y = 1 / (height * pixelRatio);
+
+            skinViewer.render = () => {
+                skinViewer.playerObject.lookAt(new Vector3(12, 0, 24));
+                composer.render();
+            };
         
-        const composer = new EffectComposer(skinViewer.renderer);
-        const renderPass = new RenderPass(skinViewer.scene, skinViewer.camera);
-        const fxaaPass = new ShaderPass(FXAAShader);
-        composer.addPass(renderPass);
-        composer.addPass(fxaaPass);
+            skinViewer.controls.enablePan = false;
+            skinViewer.controls.enableZoom = zoom;
+            skinViewer.controls.autoRotate = false;
+            skinViewer.controls.rotateSpeed = .25;
+            skinViewer.controls.enableRotate = control;
 
-        composer.setSize(width, height);
-        const pixelRatio = skinViewer.renderer.getPixelRatio();
-        composer.setPixelRatio(pixelRatio);
-        fxaaPass.material.uniforms['resolution'].value.x = 1 / (width * pixelRatio);
-        fxaaPass.material.uniforms['resolution'].value.y = 1 / (height * pixelRatio);
+            if (background === 'none')
+                skinViewer.renderer.setClearColor(0xffffff, 0);
+            else 
+                skinViewer.background = background;
+        
+            if (walk) 
+                skinViewer.animation = new WalkAnimation();
+            if (stillWalk)
+                skinViewer.animation = new WalkAnimationStill();
 
-        skinViewer.render = () => {
-            composer.render();
-            skinViewer.playerObject.lookAt(new Vector3(12, 0, 24));
-        };
-    
-        skinViewer.controls.enablePan = false;
-        skinViewer.controls.enableZoom = zoom;
-        skinViewer.controls.autoRotate = false;
-        skinViewer.controls.rotateSpeed = .25;
-        skinViewer.controls.enableRotate = control;
+            if (image) {
+                skinViewer.loadSkin(skin, {
+                    model
+                }).then(async() => {
+                    if (cape)
+                        await skinViewer.loadCape(cape);
+                    skinViewer.animation?.update(skinViewer.playerObject, 0);
+                    skinViewer.render();
 
-        if (background === 'none')
-            skinViewer.renderer.setClearColor(0xffffff, 0);
-        else 
-            skinViewer.background = background;
-    
-        if (walk) 
-            skinViewer.animation = new WalkAnimation();
-        if (stillWalk)
-            skinViewer.animation = new WalkAnimationStill();
-    
-        return () => skinViewer.dispose();
+                    const img = canvas.current;
+                    if (img) {
+                        img.src = skinViewer.canvas.toDataURL();
+                        img.width = skinViewer.width;
+                        img.height = skinViewer.height;
+                    }
+
+                    skinViewer.dispose();
+                });
+            }
+        
+            skinViewer.renderer.forceContextRestore();
+            return () => {
+                skinViewer.dispose();
+            };
+        } catch(err) {
+            console.log(err);
+        }
     }, [skin, cape, model, width, height, control, walk, background]);
-    return <canvas ref={canvas}/>
+    if (image)
+        return <img ref={canvas}/>
+    else
+        return <canvas ref={canvas}/>;
 };
 class WalkAnimation extends PlayerAnimation {
     animate({ skin }) {
