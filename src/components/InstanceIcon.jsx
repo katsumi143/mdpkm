dda7e156b3494e32d72134b2e89747dc469ddd00
Blookers <blookyl305@gmail.com>
2022-10-23 16:18:48 +1000

    hmmm...

--- src/components/InstanceIcon.jsx
+++ src/components/InstanceIcon.jsx
@@ -3,11 +3,9 @@ import React, { useState } from 'react';
 import ImagePreview from './ImagePreview';
 import ImageTransition from './Transition/Image';
 
-import API from '../common/api';
 import Patcher from '/src/common/plugins/patcher';
-export default Patcher.register(function InstanceIcon({ instance, size, hideLoader, props }) {
+export default Patcher.register(function InstanceIcon({ instance, size, props }) {
     const [preview, setPreview] = useState(false);
-    const loaderIcon = API.getLoader(instance?.config?.loader?.type)?.icon;
     size = `${size ?? 48}px`;
     return <React.Fragment>
         <ImageTransition src={instance.webIcon} onClick={() => setPreview(true)} background="$secondaryBackground" borderRadius="8.33333333%" {...props} css={{
@@ -19,22 +17,6 @@ export default Patcher.register(function InstanceIcon({ instance, size, hideLoad
             minHeight: size,
             boxShadow: '$buttonShadow',
             
-            '&:after': hideLoader || !loaderIcon ? undefined : {
-                top: 0,
-                left: 0,
-                width: '1.2rem',
-                height: '1.2rem',
-                border: '$primaryBackground solid 2px',
-                content: '',
-                display: 'block',
-                position: 'relative',
-                transform: 'translate(175%, 175%)',
-                borderRadius: '50%',
-                backgroundSize: 'contain',
-                backgroundColor: '$primaryBackground',
-                backgroundImage: `url('${loaderIcon}')`,
-                backgroundRepeat: 'no-repeat'
-            },
             ...props?.css
         }}/>
         {preview && <ImagePreview src={instance.webIcon} size={192} onClose={() => setPreview(false)}/>}
