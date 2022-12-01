b103f75e2333907f5174fa43c9f8c3bcf113ec0a
Blookers <blookyl305@gmail.com>
2022-12-02 01:37:33 +1000

    interface: fix VersionPicker react hook error

--- src/interface/components/VersionPicker.tsx
+++ src/interface/components/VersionPicker.tsx
@@ -34,21 +34,22 @@ export default function VersionPicker({ id, value, versions, onChange }: Version
                     }
                 }}>
                     <Typography size={14}>
-                        {t(`voxura:loader.${id}.release_category.${key}`)}
+                        {t(`voxura:component.${id}.release_category.${key}`)}
                     </Typography>
                     <Typography size={12} color={category === key ? undefined : '$secondaryColor'}>
-                        {items.length} Items
+                        {t('interface:common.label.item_count', [items.length])}
                     </Typography>
                 </Grid>
             )}
         </Grid>}
         <Grid width={versions.length > 1 ? '60%' : '100%'} spacing={4} padding={8} vertical>
             <Typography size={20} height="fit-content" margin="4px 0 12px 4px">
-                {t(`voxura:loader.${id}.versions`)}
+                {t(`voxura:component.${id}.versions`)}
             </Typography>
             <Grid spacing={4} vertical borderRadius={8} css={{ overflow: 'auto' }}>
-                {versions[category].map((item, key) =>
-                    <Grid key={key} padding="4px 12px" onClick={() => onChange(item)} borderRadius={8} justifyContent="space-between" css={{
+                {versions[category].map((item, key) => {
+					const [day, data] = getDayString(item.dateCreated);
+                    return <Grid key={key} padding="4px 12px" onClick={() => onChange(item)} borderRadius={8} justifyContent="space-between" css={{
                         cursor: 'pointer',
                         boxShadow: value === item ? '$buttonShadow' : undefined,
                         background: value === item ? '$buttonBackground' : '$secondaryBackground',
@@ -57,28 +58,28 @@ export default function VersionPicker({ id, value, versions, onChange }: Version
                         }
                     }}>
                         <Typography size={14}>
-                            {t(`voxura:loader.${id}.release_category.${category}.singular`)} {item.id}
+                            {t(`voxura:component.${id}.release_category.${category}.singular`)} {item.id}
                         </Typography>
                         <Typography size={12} color={value === item ? undefined : '$secondaryColor'}>
-                            Released {getDayString(item.dateCreated)}
+                            {t(`interface:common.date.${day}`, data)}
                         </Typography>
                     </Grid>
-                )}
+				})}
             </Grid>
         </Grid>
     </Grid>;
 };
 
-function getDayString(date: Date) {
+function getDayString(date: Date): [string, number[]?] {
     const difference = Date.now() - date.getTime();
     const days = Math.floor(difference / (1000 * 3600 * 24));
     if (days === 0)
-        return 'today';
+        return ['released_today'];
     if (days === 1)
-        return 'yesterday';
+        return ['released_yesterday'];
     if (days >= 365)
-        return `${Math.floor(days / 365)} years ago`;
+        return ['released_years', [Math.floor(days / 365)]];
     if (days >= 30)
-        return `${Math.floor(days / 30)} months ago`;
-    return `${days} days ago`;
+        return ['released_months', [Math.floor(days / 30)]];
+    return ['released_days', [Math.floor(days)]];
 };
