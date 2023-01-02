0d222104f207cf29d8f0c8c81dfa481a2e946164
Blookers <blookyl305@gmail.com>
2023-01-02 14:53:13 +1000

    disable default context menu

--- src/interface/pages/navigation.tsx
+++ src/interface/pages/navigation.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { styled } from '@stitches/react';
 import { Toaster } from 'react-hot-toast';
 import { useTranslation } from 'react-i18next';
 import { Main, SideNavigation, NavigationItem } from 'voxeliface';
@@ -27,52 +28,61 @@ export default function Navigation() {
 
     const changePage = (page: string) => dispatch(setPage(page));
     return <App>
-        <Header/>
-        <Main css={{
-            padding: 0,
-            overflow: 'hidden auto',
-            flexDirection: 'row'
-        }}>
-            <SideNavigation value={page} onChange={changePage} css={{
-                '& > *:first-child': {
-                    display: SHOULD_HIDE_SIDE.some(p => p === page) ? 'none' : undefined
-                }
-            }}>
-                <NavigationItem name={t('navigation.home')} icon={<IconBiHouse/>} value="home" direction="horizontal">
-                    <Home/>
-                </NavigationItem>
-                <NavigationItem name={t('navigation.instances')} icon={<IconBiListUl/>} value="instances" direction="horizontal">
-                    <Instances/>
-                </NavigationItem>
-                <NavigationItem name={t('navigation.skins')} icon={<IconBiPersonBadge/>} value="skins">
-                    <Skins/>
-                </NavigationItem>
-                <NavigationItem name={t('navigation.import')} icon={null} value="import" hidden>
-                    <Import/>
-                </NavigationItem>
-                <NavigationItem name={t('navigation.create')} icon={null} value="create" hidden>
-                    <Create/>
-                </NavigationItem>
-				<NavigationItem name={t('navigation.mod_search')} icon={null} value="modSearch" hidden>
-                    <ModSearch/>
-                </NavigationItem>
-                <NavigationItem name={t('navigation.developer')} icon={<IconBiWrenchAdjustableCircle/>} value="developer" hidden footer>
-                    <Developer/>
-                </NavigationItem>
-                <NavigationItem name={t('navigation.downloads')} icon={<IconBiDownload/>} value="downloads" footer>
-                    <Downloads/>
-                </NavigationItem>
-                <NavigationItem name={t('navigation.accounts')} icon={<IconBiPerson/>} value="accounts" footer>
-                    <Accounts/>
-                </NavigationItem>
-                <NavigationItem name={t('navigation.settings')} icon={<IconBiGear/>} value="settings" footer>
-                    <Settings/>
-                </NavigationItem>
-            </SideNavigation>
-            <Toaster position="bottom-right"/>
-        </Main>
-		{launchError && <LaunchError data={launchError}/>}
+		<Container onContextMenu={(e)=> e.preventDefault()}>
+			<Header/>
+			<Main css={{
+				padding: 0,
+				overflow: 'hidden auto',
+				flexDirection: 'row'
+			}}>
+				<SideNavigation value={page} onChange={changePage} css={{
+					'& > *:first-child': {
+						display: SHOULD_HIDE_SIDE.some(p => p === page) ? 'none' : undefined
+					}
+				}}>
+					<NavigationItem name={t('navigation.home')} icon={<IconBiHouse/>} value="home" direction="horizontal">
+						<Home/>
+					</NavigationItem>
+					<NavigationItem name={t('navigation.instances')} icon={<IconBiListUl/>} value="instances" direction="horizontal">
+						<Instances/>
+					</NavigationItem>
+					<NavigationItem name={t('navigation.skins')} icon={<IconBiPersonBadge/>} value="skins">
+						<Skins/>
+					</NavigationItem>
+					<NavigationItem name={t('navigation.import')} icon={null} value="import" hidden>
+						<Import/>
+					</NavigationItem>
+					<NavigationItem name={t('navigation.create')} icon={null} value="create" hidden>
+						<Create/>
+					</NavigationItem>
+					<NavigationItem name={t('navigation.mod_search')} icon={null} value="modSearch" hidden>
+						<ModSearch/>
+					</NavigationItem>
+					<NavigationItem name={t('navigation.developer')} icon={<IconBiWrenchAdjustableCircle/>} value="developer" hidden footer>
+						<Developer/>
+					</NavigationItem>
+					<NavigationItem name={t('navigation.downloads')} icon={<IconBiDownload/>} value="downloads" footer>
+						<Downloads/>
+					</NavigationItem>
+					<NavigationItem name={t('navigation.accounts')} icon={<IconBiPerson/>} value="accounts" footer>
+						<Accounts/>
+					</NavigationItem>
+					<NavigationItem name={t('navigation.settings')} icon={<IconBiGear/>} value="settings" footer>
+						<Settings/>
+					</NavigationItem>
+				</SideNavigation>
+				<Toaster position="bottom-right"/>
+			</Main>
+			{launchError && <LaunchError data={launchError}/>}
+		</Container>
     </App>;
 }
+
+const Container = styled('div', {
+	width: '100vw',
+	height: '100vh',
+	display: 'flex',
+	flexDirection: 'column'
+});
 
 export const SHOULD_HIDE_SIDE = ['import', 'create', 'modSearch'];
