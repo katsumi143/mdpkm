7ca07fea01ed600a0c069c3f826708a5a52419b1
Blookers <blookyl305@gmail.com>
2023-04-27 14:28:08 +1000

    remove unused tauri commands

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -22,13 +22,8 @@ fn main() {
     tauri::Builder::default()
         .plugin(voxura::init())
         .invoke_handler(tauri::generate_handler![
-            move_dir,
-            fs_read_dir,
 			check_for_update,
             get_total_memory,
-            fs_create_dir_all,
-            fs_read_file_in_zip,
-            fs_read_dir_recursive,
 
 			cmd::copy_dir,
 			cmd::read_text_file_in_zip,
@@ -65,65 +60,6 @@ fn check_for_update(app_handle: tauri::AppHandle) {
 	tauri::async_runtime::spawn(async move {
 		tauri::updater::builder(app_handle.clone()).should_install(|_current, _latest| true).check().await.unwrap();
 	});
-}
-
-#[tauri::command]
-fn move_dir(path: String, target: String) {
-    std::fs::rename(path, target).ok();
-}
-
-#[tauri::command]
-fn fs_create_dir_all(path: String) {
-    std::fs::create_dir_all(path).ok();
-}
-
-#[tauri::command]
-fn fs_read_file_in_zip(path: String, file_path: String) -> Result<String, String> {
-    let file = std::fs::File::open(std::path::Path::new(&*path));
-
-    if file.is_ok() {
-        let archiv = zip::ZipArchive::new(file.as_ref().unwrap());
-        if archiv.is_ok() {
-            let mut archive = archiv.unwrap();
-            for i in 0..archive.len() {
-                let mut file = archive.by_index(i).unwrap();
-                if file.name().contains(&*file_path) {
-                    let mut buf = String::new();
-                    file.read_to_string(&mut buf).unwrap();
-                    return Ok(buf);
-                }
-            }
-            return Err("File does not exist".to_string());
-        }
-        return Err(archiv.unwrap_err().to_string());
-    }
-    return Err(file.unwrap_err().to_string());
-}
-
-use std::io::{ Read };
-use walkdir::{ WalkDir };
-
-#[tauri::command]
-fn fs_read_dir(path: String) -> Result<Vec<Vec<String>>, String> {
-    let read = std::fs::read_dir(path);
-    if read.is_ok() {
-        return Ok(read.unwrap().filter_map(| entry | {
-            entry.ok().and_then(| e |
-                Some(vec![e.path().into_os_string().into_string().unwrap(), e.file_type().unwrap().is_dir().to_string()])
-            )
-        }).collect::<Vec<Vec<String>>>());
-    }
-    return Err(read.unwrap_err().to_string());
-}
-
-#[tauri::command]
-fn fs_read_dir_recursive(path: String) -> Result<Vec<Vec<String>>, String> {
-    let walkdir = WalkDir::new(path);
-    return Ok(walkdir.into_iter().filter_map(| entry | {
-        entry.ok().and_then(| e |
-            Some(vec![e.path().to_str().unwrap().to_owned().to_string(), e.file_type().is_dir().to_string()])
-        )
-    }).collect::<Vec<Vec<String>>>());
 }
 
 #[tauri::command]
